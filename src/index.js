310d3d3705177ef4bbd601744222878b7e53de27
Faith <fthmorgan4@gmail.com>
2023-09-19 14:23:29 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 function Greeting() {
   return (
     <React.Fragment>
-      <div>
+      <div className="someValue">
         <h2>My First Component</h2>
         <ul>
           <li>
