e1846b2272feff80067042a246561dd09c682440
Faith <fthmorgan4@gmail.com>
2023-09-19 15:46:36 -0600

    page styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const BookList = () => {
-  return <section>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+  return <section className="booklist">
     <Book />
     <Book />
     <Book />
@@ -16,17 +12,17 @@ const BookList = () => {
 }
 
 const Book = () => {
-  return <article>
+  return <article className="book">
     <Image />
     <Title />
     <Author />
   </article>
 }
 
-const Image = () => <h2>image placeholder</h2>
-const Title = () => <h2>Book Title</h2>
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL900_SR900,600_.jpg" alt="Build the Life You Want: The Art and Science of Getting" />
+const Title = () => <h2>Build the Life You Want: The Art and Science of Getting</h2>
 const Author = () => {
-  return <h4>Author</h4>;
+  return <h4>Arthur C. Brooks</h4>;
 }
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
