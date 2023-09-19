9fba6416368796c62d6f6e2acd5618cad6604773
Faith <fthmorgan4@gmail.com>
2023-09-19 15:31:21 -0600

    Created Book component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
-// function Greeting() {
-//   return (
-//     <React.Fragment>
-//       <div className="someValue">
-//         <h2>My First Component</h2>
-//         <ul>
-//           <li>
-//             <a href="#">hello world</a>
-//           </li>
-//         </ul>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
-// function Greeting() {
-//   return React.createElement('h2', {}, 'hello world');
-// }
-
-// nested component 
-function Greeting() {
-  return <div>
-    <Person />
-    <Message />
-  </div>;
+const BookList = () => {
+  return <section>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
 }
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <h2>this is my message</h2>;
-};
+const Book = () => {
+  return <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+}
 
-
+const Image = () => <h2>image placeholder</h2>
+const Title = () => <h2>Book Title</h2>
+const Author = () => {
+  return <h4>Author</h4>;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<BookList />)
