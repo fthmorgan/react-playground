b2281bcaa5528d8e29f08071fc335f6466e2b5b3
Faith <fthmorgan4@gmail.com>
2023-09-20 10:16:42 -0600

    added props

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 
 const BookList = () => {
   return <section className="booklist">
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book author={author} title={title} img={img} />
+    <Book author={author} title={title} img={img} />
   </section>
 }
+const title = 'Build the Life You Want: The Art and Science of Getting';
 const author = 'Jordan Moore';
-const Book = () => {
-  const title = 'Build the Life You Want: The Art and Science of Getting';
+const img = "https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL900_SR900,600_.jpg";
+const Book = (props) => {
+  console.log(props)
   return (
     <article className="book">
-      <img src="https://images-na.ssl-images-amazon.com/images/I/81+MJor-K6L._AC_UL900_SR900,600_.jpg" alt="Build the Life You Want: The Art and Science of Getting" />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author.toUpperCase()}</h4>
     </article>
   );
 };
